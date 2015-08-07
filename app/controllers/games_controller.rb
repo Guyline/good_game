9a80cfd384718fe195b3aadb54ea78afe020c7ed
Dan Halloran <guyline82@gmail.com>
2015-08-07 11:31:16 -0700

    Update GamesController#index, define #show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,18 @@ class GamesController < ApplicationController
     search_query = params[:query]
     page = params[:page] || 1
     per_page = params[:per_page] || 30
-    @games = Games.paginate(:page => page, :per_page => per_page)
-    @games = @games.where("name LIKE ?", "%#{search_query}%") if search_query.present?
+    @games = Game.includes(:platforms, :publishers, :developers).paginate(:page => page, :per_page => per_page)
+    @games = @games.where("games.name LIKE ?", "%#{search_query}%") if search_query.present?
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def show
+    @game = Game.where(:id => params[:id]).
+      includes(:platforms, :publishers, :developers).
+      first
 
     respond_to do |format|
       format.html
